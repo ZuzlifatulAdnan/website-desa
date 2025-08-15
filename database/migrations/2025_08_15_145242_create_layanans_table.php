b4629bfb368defa690946a56e6c05f763940dd08
ZuzlifatulAdnan <juslifatuladnan@gmail.com>
2025-08-15 22:16:24 +0700

    migration database

---
+++ database/migrations/2025_08_15_145242_create_layanans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('layanans', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('slug')->unique();
+            $table->string('icon');
+            $table->text('deskripsi');
+            $table->text('persyaratan');
+            $table->text('prosedur');
+            $table->string('waktu_pelayanan');
+            $table->string('biaya');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('layanans');
+    }
+};
---
+++ database/migrations/2025_08_15_145256_create_wisatas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wisatas', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('slug')->unique();
+            $table->text('deskripsi');
+            $table->string('gambar');
+            $table->time('jam_buka');
+            $table->time('jam_tutup');
+            $table->string('harga_tiket');
+            $table->string('fasilitas');
+            $table->text('lokasi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wisatas');
+    }
+};
